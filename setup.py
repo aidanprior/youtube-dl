9843e7b8d34511aa32a0106aa1c69c0392c0b6db
Aidan Prior <priorlyaidan@yahoo.com>
2020-07-12 14:30:36 -0400

    maybe this will work better?

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ setup(
     url="https://github.com/aidanprior/youtube-dl",
     version='2.0',
     packages=['youtube-dl-scripts'],
-    data_files=[("youtube-dl-scripts/Logs", ['YT_video_playlists.log', 'YTMusic_playlist_to_mp3s.log', ]),
-                ("youtube-dl-scripts/Logs", ['Audio_Archive.txt', 'Video_Archive.txt', ]),
-                ("youtube-dl-scripts", ['config.cfg', ])
+    data_files=[("Logs", ['youtube-dl-scripts/Logs/YT_video_playlists.log', 'youtube-dl-scripts/Logs/YTMusic_playlist_to_mp3s.log', ]),
+                ("Archives", ['youtube-dl-scripts/Archives/Audio_Archive.txt', 'youtube-dl-scripts/Archives/Video_Archive.txt', ]),
+                ("", ['youtube-dl-scripts/config.ini', ])
                 ],
     install_requires = ['youtube_dl',],
     license='GNU GPL3',
