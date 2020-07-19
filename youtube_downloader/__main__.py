23a92cb25c7554dc9a08bc62bd4dea8650327fce
Aidan Prior <priorlyaidan@yahoo.com>
2020-07-19 19:07:04 -0400

    fixed -c arg parsing

--- youtube_downloader/__main__.py
+++ youtube_downloader/__main__.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 if __name__ == "__main__":
     parser = ArgumentParser(prog="Youtube Downloader", description="A simple GUI app for downloading videos and mp3s from Youtube with youtube-dl")
-    parser.add_argument("-c", "--config", metavar="CONFIG_FILE", action='store_const', const="EMPTY", help="The .cfg file, located in the 'User Options' folder, to load the application with")
+    parser.add_argument("-c", "--config", metavar="CONFIG_FILE", const="EMPTY", help="The .cfg file, located in the 'User Options' folder, to load the application with")
     parser.add_argument("-l", "--log", metavar="LOG_FILE", default=str(Path(__file__).parent / "data/stdout.log"), help="The file to log stdout and stderr to")
     args = parser.parse_args()
     
