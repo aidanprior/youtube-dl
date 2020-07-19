bb90fbc25434abd80bc64898c9641f209719903c
Aidan Prior <priorlyaidan@yahoo.com>
2020-07-19 02:16:01 -0400

    final fixup

--- youtube_downloader/__init__.py
+++ youtube_downloader/__init__.py
@@ -7,7 +7,7 @@ from argparse import ArgumentParser
 if __name__ == "__main__":
     parser = ArgumentParser(prog="Youtube Downloader", description="A simple GUI app for downloading videos and mp3s from Youtube with youtube-dl")
     parser.add_argument("-c", "--config", metavar="CONFIG_FILE", help="The .cfg file to load the application with")
-    parser.add_argument("-l", "--log", metavar="LOG_FILE" action='store_const', const="data/stdout.log", help="The file to log stdout and stderr to")
+    parser.add_argument("-l", "--log", metavar="LOG_FILE", action='store_const', const="data/stdout.log", help="The file to log stdout and stderr to")
     args = parser.parse_args()
     cmd = [
         sys.executable, 
